c849be5838580dc281c6d6e7c5f389a04390a736
Elisa <elisas@itcsa.net.ar>
2020-01-29 18:44:45 -0300

    ejemploInyeccionDependencia

---
+++ demo/src/main/java/com/dh/demo/entidades/MiEntidad.java
@@ -0,0 +1,31 @@
+package com.dh.demo.entidades;
+
+
+
+public class MiEntidad {
+
+	private Long id;
+	private String name;
+	private Integer cantidad;
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+
+		
+}
